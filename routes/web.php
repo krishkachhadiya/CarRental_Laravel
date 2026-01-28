494edad6026d2161a893486c47823d0a4e4fa8d1
krish <krishkachhadiya20@gmail.com>
2026-01-28 11:12:27 +0530

    update web.php add vehicle route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\frontend\aboutController;
 
+use App\Http\Controllers\frontend\homeController;
 use App\Http\Controllers\frontend\loginController;
 use App\Http\Controllers\frontend\registerationController;
-
+use App\Http\Controllers\frontend\vehicleController;
 
 
 Route::get('/login', [loginController::class, 'index'])->name('login');
@@ -32,4 +33,18 @@ Route::get('/about', function () {
         return redirect('/login');
     }
     return app(aboutController::class)->index();
+});
+
+
+Route::get('/vehicle', function () {
+    if (!session('user_login')) {
+        return redirect('/login');
+    }
+    return app(VehicleController::class)->index();
+});
+
+Route::middleware(['auth'])->group(function () {
+
+
+   
 });
