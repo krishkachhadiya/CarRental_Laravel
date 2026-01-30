c515fd79f599f7b7522449679b85b7f8bfdb02fe
krish <krishkachhadiya20@gmail.com>
2026-01-30 18:31:59 +0530

    add a blog controller

---
+++ app/Http/Controllers/frontend/blogController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\frontend;
+
+use App\Http\Controllers\Controller;
+
+
+class blogController extends Controller
+{
+    public function index()
+    {
+        return view('frontend.blog');
+    }
+}
