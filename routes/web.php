4a6c2b6fe86042677131b24b0c79b30a8831268c
krish <krishkachhadiya20@gmail.com>
2026-01-30 18:34:21 +0530

    update and add team route

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\frontend\aboutController;
-
+use App\Http\Controllers\frontend\blogController;
+use App\Http\Controllers\frontend\bookController;
+use App\Http\Controllers\frontend\contactController;
 use App\Http\Controllers\frontend\homeController;
 use App\Http\Controllers\frontend\loginController;
 use App\Http\Controllers\frontend\registerationController;
+use App\Http\Controllers\frontend\teamController;
 use App\Http\Controllers\frontend\vehicleController;
+use App\Http\Controllers\frontend\BookingController;
 
+// Book Now page
+Route::get('/book', [BookingController::class, 'index']);
+Route::post('/book-now', [BookingController::class, 'store']);
+Route::get('/my-bookings', [BookingController::class, 'myBookings']); // Show bookings page
 
 Route::get('/login', [loginController::class, 'index'])->name('login');
 Route::post('/login', [loginController::class, 'login']);
@@ -35,6 +43,33 @@ Route::get('/about', function () {
     return app(aboutController::class)->index();
 });
 
+Route::get('/blog', function () {
+    if (!session('user_login')) {
+        return redirect('/login');
+    }
+    return app(blogController::class)->index();
+});
+
+Route::get('/book', function () {
+    if (!session('user_login')) {
+        return redirect('/login');
+    }
+    return app(BookController::class)->index();
+});
+
+Route::get('/contact', function () {
+    if (!session('user_login')) {
+        return redirect('/login');
+    }
+    return app(ContactController::class)->index();
+});
+
+Route::get('/team', function () {
+    if (!session('user_login')) {
+        return redirect('/login');
+    }
+    return app(TeamController::class)->index();
+});
 
 Route::get('/vehicle', function () {
     if (!session('user_login')) {
